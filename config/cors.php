ec099d66314aef6ee140b133cfb818ab898f8abb
Muhammad Zaid Al Anshori <zaid.al.anshori@gmail.com>
2020-04-03 07:50:08 +0700

    Handling CORS 2

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => ['*'],
+    'paths' => ['api/*'],
 
     /*
     * Matches the request method. `[*]` allows all methods.
